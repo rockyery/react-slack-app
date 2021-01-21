651b1c7ee500ad73d096471cffc30e1f1707fa6e
rockyery <38206170+rockyery@users.noreply.github.com>
2021-01-21 22:12:43 +0800

    import semantic, add firebase and enable user registration

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyC9BFwuXV3bgIWdF06wP5eADo27LUDIJ_w',
+  authDomain: 'react-slack-app-f981e.firebaseapp.com',
+  projectId: 'react-slack-app-f981e',
+  storageBucket: 'react-slack-app-f981e.appspot.com',
+  messagingSenderId: '485928686359',
+  appId: '1:485928686359:web:bd041278fdb43b0cfd1d52',
+  measurementId: 'G-8PF04SZCFQ',
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
