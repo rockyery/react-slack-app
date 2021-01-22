485cd25092a5d5fa23af06593973768df5140630
rockyery <38206170+rockyery@users.noreply.github.com>
2021-01-22 19:40:17 +0800

    display avatar

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dropdown, Grid, Header, Icon } from 'semantic-ui-react'
+import { Dropdown, Grid, Header, Icon, Image } from 'semantic-ui-react'
 import firebase from '../../firebase'
 
 class UserPanel extends React.Component {
@@ -38,7 +38,7 @@ class UserPanel extends React.Component {
   }
 
   render() {
-    console.log(this.props.currentUser)
+    const { user } = this.state
     return (
       <Grid style={{ background: '#4c3c4c' }}>
         <Grid.Column>
@@ -53,7 +53,12 @@ class UserPanel extends React.Component {
           {/* User Dropdown */}
           <Header style={{ padding: '0.25em' }} as='h4' inverted>
             <Dropdown
-              trigger={<span>{this.state.user.displayName}</span>}
+              trigger={
+                <span>
+                  <Image src={user.photoURL} spaced='right' avatar />
+                  {this.state.user.displayName}
+                </span>
+              }
               options={this.dropdownOptions()}
             />
           </Header>
