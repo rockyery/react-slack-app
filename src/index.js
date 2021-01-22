5cc022ea1f56103cd62aa9b68875f83fd669e2d2
rockyery <38206170+rockyery@users.noreply.github.com>
2021-01-22 17:44:50 +0800

    setup Redux and make user available on global state

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+
 import registerServiceWorker from './registerServiceWorker'
 import firebase from './firebase'
 import 'semantic-ui-css/semantic.min.css'
@@ -12,13 +15,19 @@ import {
   Route,
   withRouter,
 } from 'react-router-dom'
-import Login from './components/Auth/Login'
-import Register from './components/Auth/Register'
+
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './reducers'
+import { setUser } from './actions'
+const store = createStore(rootReducer, composeWithDevTools())
 
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
+        this.props.setUser(user)
         this.props.history.push('/')
       }
     })
@@ -35,11 +44,14 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root)
+const RootWithAuth = withRouter(connect(null, { setUser })(Root))
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+
   document.getElementById('root')
 )
 registerServiceWorker()
