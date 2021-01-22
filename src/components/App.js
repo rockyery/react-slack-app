7c328bf6285c3a37ab41879a83dc025f0bf9071f
rockyery <38206170+rockyery@users.noreply.github.com>
2021-01-22 18:05:35 +0800

    Scaffold app components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
-import logo from '../logo.svg'
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
 import './App.css'
 
-class App extends Component {
-  render() {
-    return <div>App</div>
-  }
+import ColorPanel from './ColorPanel/ColorPanel'
+import Messages from './Messages/Messages'
+import MetaPanel from './MetaPanel/MetaPanel'
+import SidePanel from './SidePanel/SidePanel'
+
+const App = () => {
+  return (
+    <Grid columns='equal' className='app' style={{ background: '#eee' }}>
+      <ColorPanel />
+      <SidePanel />
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages />
+      </Grid.Column>
+
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
+  )
 }
 
 export default App
