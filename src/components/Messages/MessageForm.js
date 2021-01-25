b015526049595a384ba9fbbf00391e17da31d139
rockyery <38206170+rockyery@users.noreply.github.com>
2021-01-26 00:43:49 +0800

    add checking for progress bar visibility

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -93,6 +93,7 @@ class MessageForm extends React.Component {
           (snap) => {
             const percentUploaded =
               (snap.bytesTransferred / snap.totalBytes) * 100
+            this.props.isProgressBarVisible(percentUploaded)
             this.setState({ percentUploaded })
           },
           (err) => {
