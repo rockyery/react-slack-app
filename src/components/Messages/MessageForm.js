b563993e7d9d08396017727819f1e137ad65dec5
rockyery <38206170+rockyery@users.noreply.github.com>
2021-01-26 00:24:53 +0800

    add upload progress bar

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -3,6 +3,7 @@ import uuidv4 from 'uuid/v4'
 import firebase from '../../firebase'
 import { Segment, Button, Input } from 'semantic-ui-react'
 import FileModal from './FileModal'
+import ProgressBar from './ProgressBar'
 
 class MessageForm extends React.Component {
   state = {
@@ -139,7 +140,14 @@ class MessageForm extends React.Component {
   }
 
   render() {
-    const { errors, message, loading, modal } = this.state
+    const {
+      errors,
+      message,
+      loading,
+      modal,
+      uploadState,
+      percentUploaded,
+    } = this.state
 
     return (
       <Segment className='message__form'>
@@ -174,12 +182,16 @@ class MessageForm extends React.Component {
             labelPosition='right'
             icon='cloud upload'
           />
-          <FileModal
-            modal={modal}
-            closeModal={this.closeModal}
-            uploadFile={this.uploadFile}
-          />
         </Button.Group>
+        <FileModal
+          modal={modal}
+          closeModal={this.closeModal}
+          uploadFile={this.uploadFile}
+        />
+        <ProgressBar
+          uploadState={uploadState}
+          percentUploaded={percentUploaded}
+        />
       </Segment>
     )
   }
