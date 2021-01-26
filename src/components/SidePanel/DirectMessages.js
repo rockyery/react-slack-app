15205e558191c6b822fd8fd262eb3e6e061eaf55
rockyery <38206170+rockyery@users.noreply.github.com>
2021-01-26 23:08:32 +0800

    display as active the current active DM channel

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -76,6 +76,7 @@ class DirectMessages extends React.Component {
     }
     this.props.setCurrentChannel(channelData)
     this.props.setPrivateChannel(true)
+    this.setActiveChannel(user.uid)
   }
 
   getChannelId = (userId) => {
@@ -85,8 +86,12 @@ class DirectMessages extends React.Component {
       : `${currentUserId}/${userId}`
   }
 
+  setActiveChannel = (userId) => {
+    this.setState({ activeChannel: userId })
+  }
+
   render() {
-    const { users } = this.state
+    const { users, activeChannel } = this.state
 
     return (
       <Menu.Menu className='menu'>
@@ -99,6 +104,7 @@ class DirectMessages extends React.Component {
         {users.map((user) => (
           <Menu.Item
             key={user.uid}
+            active={user.uid === activeChannel}
             onClick={() => this.changeChannel(user)}
             style={{ opacity: 0.7, fontStyle: 'italic' }}
           >
